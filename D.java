62031cd09599192f4dadfb34d9e7499fb072f123
Chaitra111 <111chaitra@gmail.com>
2018-09-18 13:05:50 +0530

    jenkinks

---
+++ D.java
@@ -0,0 +1,23 @@
+class D
+{
+	public static void main(String[]args)
+	{
+	int spaces=8;
+	for(int i=1;i<=9;i++)
+	{	
+		for(int j=1;j<=14;j++)
+		{
+			if(j==1||j==2|| (i==1||i==9)&&j<=10)
+			{
+				System.out.print("*");
+			}
+		}
+	
+System.out.println("");
+	}
+	
+	
+	}
+}
+
+
